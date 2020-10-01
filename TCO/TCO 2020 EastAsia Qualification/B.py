265a39cdc6d0e427018d2b127f80288196272ed5
songzy <songzy.thu@qq.com>
2020-10-01 15:48:17 +0800

    Thu Oct  1 15:48:16 CST 2020

---
+++ TCO/TCO 2020 EastAsia Qualification/B.py
@@ -0,0 +1,4 @@
+# https://community.topcoder.com/stat?c=round_overview&er=5&rd=18266
+
+# For each possible a and b,
+# the c that leads to the minimum badness would be the median.
---
+++ TCO/TCO 2020 EastAsia Qualification/C.py
@@ -0,0 +1,3 @@
+# https://community.topcoder.com/stat?c=round_overview&er=5&rd=18266
+
+# We can just use DFS.
